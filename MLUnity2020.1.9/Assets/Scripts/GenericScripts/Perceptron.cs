d23d9988b4bfcc9c1ae188fc8fdf179bb2dcd4bf
Kylo <tomy_ramdiz@outlook.com>
2020-10-16 17:43:15 -0500

    Script basica de Perceptron creada

--- MLUnity2020.1.9/Assets/Scripts/GenericScripts/Perceptron.cs
+++ MLUnity2020.1.9/Assets/Scripts/GenericScripts/Perceptron.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Perceptron : MonoBehaviour
+public class Perceptron
 {
-    // Start is called before the first frame update
-    void Start()
+    float lr;
+    float [] w;
+    public Perceptron(int nInputs,float learningRate)
     {
-        
+        w = new float[nInputs];
+        lr = learningRate;
+        for (int i = 0; i < nInputs; i++)
+        {
+            w[i] = Random.Range(-1f, 1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Predict(float [] inputs)
     {
-        
+        float sum = 0;
+        for (int i = 0; i < w.Length; i++)
+        {
+            sum += w[i] * inputs[i];
+        }
+
+        return Activation(sum);
     }
+
+    public void supervisedTrain(float[] inputs, int target)
+    {
+        int predicted = Predict(inputs);
+        int error = target - predicted;
+        for (int i = 0; i < w.Length; i++)
+        {
+            w[i] += error * inputs[i] * lr;
+        }
+    }
+
+    int Activation(float sum)
+    {
+        if (sum >= 0)
+            return 1;
+        return -1;
+    }
+
 }
