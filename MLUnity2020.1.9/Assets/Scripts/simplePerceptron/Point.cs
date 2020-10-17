02ded7c3327b9221cf0066e5c9890aa5d70ed198
Kylo <tomy_ramdiz@outlook.com>
2020-10-17 16:37:31 -0500

    Escena basica

--- MLUnity2020.1.9/Assets/Scripts/simplePerceptron/Point.cs
+++ MLUnity2020.1.9/Assets/Scripts/simplePerceptron/Point.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Point : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
--- MLUnity2020.1.9/Assets/Scripts/simplePerceptron/SceneManagment.cs
+++ MLUnity2020.1.9/Assets/Scripts/simplePerceptron/SceneManagment.cs
@@ -1,10 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SceneManagment : MonoBehaviour
 {
     public GameObject pointPrefab;
+    public int nPoints;
+    public float lr,depth;
+    Vector2 LeftBottom,rightTop;
+    List<GameObject> points;
+    Perceptron p;
+
+    private void Awake()
+    {
+        points = new List<GameObject>();
+        p = new Perceptron(nPoints, lr);
+
+        LeftBottom = Camera.main.ScreenToWorldPoint(new Vector3(0,0,depth));
+        rightTop = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,depth));
+        for (int i = 0; i < nPoints; i++)
+        {
+            GameObject obj = Instantiate(pointPrefab, new Vector3(Random.Range(LeftBottom.x, rightTop.x), Random.Range(LeftBottom.y, rightTop.y), 2),Quaternion.identity);
+            points.Add(obj);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +36,6 @@ public class SceneManagment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+  
     }
 }
